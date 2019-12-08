e1cfc4741dd4468bf9cd206ebdf2cd0a147c62dd
Dinah Baum <33238993+DBaum1@users.noreply.github.com>
2019-12-08 13:45:15 -0600

    Fix formatting

--- src/ace_runner.cpp
+++ src/ace_runner.cpp
@@ -13,40 +13,39 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-
 AbstractAceRunner::AbstractAceRunner(std::string testDirectory) {
-	testDir = testDirectory;
-	actionHandlers.emplace("checkpoint", &AbstractAceRunner::handle_checkpoint);
-	actionHandlers.emplace("close", &AbstractAceRunner::handle_close);
-	actionHandlers.emplace("falloc", &AbstractAceRunner::handle_falloc);
-	actionHandlers.emplace("fdatasync", &AbstractAceRunner::handle_fdatasync);
-	actionHandlers.emplace("fsync", &AbstractAceRunner::handle_fsync);
-	actionHandlers.emplace("mkdir", &AbstractAceRunner::handle_mkdir);
-	actionHandlers.emplace("open", &AbstractAceRunner::handle_open);
-	actionHandlers.emplace("opendir", &AbstractAceRunner::handle_opendir);
-	actionHandlers.emplace("remove", &AbstractAceRunner::handle_remove);
-	actionHandlers.emplace("rename", &AbstractAceRunner::handle_rename);
-	actionHandlers.emplace("sync", &AbstractAceRunner::handle_sync);
-	actionHandlers.emplace("truncate", &AbstractAceRunner::handle_truncate);
-	actionHandlers.emplace("unlink", &AbstractAceRunner::handle_unlink);
-	actionHandlers.emplace("write", &AbstractAceRunner::handle_write);
+    testDir = testDirectory;
+    actionHandlers.emplace("checkpoint", &AbstractAceRunner::handle_checkpoint);
+    actionHandlers.emplace("close", &AbstractAceRunner::handle_close);
+    actionHandlers.emplace("falloc", &AbstractAceRunner::handle_falloc);
+    actionHandlers.emplace("fdatasync", &AbstractAceRunner::handle_fdatasync);
+    actionHandlers.emplace("fsync", &AbstractAceRunner::handle_fsync);
+    actionHandlers.emplace("mkdir", &AbstractAceRunner::handle_mkdir);
+    actionHandlers.emplace("open", &AbstractAceRunner::handle_open);
+    actionHandlers.emplace("opendir", &AbstractAceRunner::handle_opendir);
+    actionHandlers.emplace("remove", &AbstractAceRunner::handle_remove);
+    actionHandlers.emplace("rename", &AbstractAceRunner::handle_rename);
+    actionHandlers.emplace("sync", &AbstractAceRunner::handle_sync);
+    actionHandlers.emplace("truncate", &AbstractAceRunner::handle_truncate);
+    actionHandlers.emplace("unlink", &AbstractAceRunner::handle_unlink);
+    actionHandlers.emplace("write", &AbstractAceRunner::handle_write);
 }
 
 
 const std::set<std::string> AbstractAceRunner::getAllFilePaths(void) {
-	std::set<std::string> paths;
-	paths.insert(testDir + "/A/C/foo");
-	paths.insert(testDir + "/A/C/bar");
-	paths.insert(testDir + "/A/foo");
-	paths.insert(testDir + "/B/foo");
-	paths.insert(testDir + "/A/bar");
-	paths.insert(testDir + "/B/bar");
-	paths.insert(testDir + "/foo");
-	paths.insert(testDir + "/bar");
-	paths.insert(testDir + "/A/C");
-	paths.insert(testDir + "/A");
-	paths.insert(testDir + "/B");
-	return paths;
+    std::set<std::string> paths;
+    paths.insert(testDir + "/A/C/foo");
+    paths.insert(testDir + "/A/C/bar");
+    paths.insert(testDir + "/A/foo");
+    paths.insert(testDir + "/B/foo");
+    paths.insert(testDir + "/A/bar");
+    paths.insert(testDir + "/B/bar");
+    paths.insert(testDir + "/foo");
+    paths.insert(testDir + "/bar");
+    paths.insert(testDir + "/A/C");
+    paths.insert(testDir + "/A");
+    paths.insert(testDir + "/B");
+    return paths;
 }
 
 std::string AbstractAceRunner::getFilePath(std::string file) {
@@ -67,23 +66,23 @@ std::string AbstractAceRunner::getFilePath(std::string file) {
 }
 
 int AbstractAceRunner::handle_action(std::vector<std::string>& tokens) {
-	std::string action = tokens[0];
-	auto it = actionHandlers.find(action);
-	if (it == actionHandlers.end()) {
-		std::cout << "Could not find action " << action << std::endl;
-		return -1;
-	}
-	return (this->*it->second)(tokens);
+    std::string action = tokens[0];
+    auto it = actionHandlers.find(action);
+    if (it == actionHandlers.end()) {
+	std::cout << "Could not find action " << action << std::endl;
+	return -1;
+    }
+    return (this->*it->second)(tokens);
 }
 
 int AbstractAceRunner::handle_close(std::vector<std::string>& tokens) {
-	int fd = fileDescriptors[getFilePath(tokens[1])];            
-	return close(fd);
+    int fd = fileDescriptors[getFilePath(tokens[1])];            
+    return close(fd);
 }
 
 int AbstractAceRunner::handle_falloc(std::vector<std::string>& tokens) {
-	const int filenameIndex = 1;
-	const int flagIndex = 2;
+    const int filenameIndex = 1;
+    const int flagIndex = 2;
     const int offset_index = 3;
     const int length_index = 4;
 
@@ -96,19 +95,19 @@ int AbstractAceRunner::handle_falloc(std::vector<std::string>& tokens) {
 }
 
 int AbstractAceRunner::handle_fdatasync(std::vector<std::string>& tokens) {
-	return fdatasync(fileDescriptors[getFilePath(tokens[1])]);
+    return fdatasync(fileDescriptors[getFilePath(tokens[1])]);
 }
 
 int AbstractAceRunner::handle_fsync(std::vector<std::string>& tokens) {
-	if (tokens[1] == "test") {
-		return fsync(test_fd);
-	} else {
-		return fsync(fileDescriptors[getFilePath(tokens[1])]);
-	}
+    if (tokens[1] == "test") {
+	return fsync(test_fd);
+    } else {
+        return fsync(fileDescriptors[getFilePath(tokens[1])]);
+    }
 }
 
 int AbstractAceRunner::handle_mkdir(std::vector<std::string>& tokens) {
-	const int dir_index = 1;
+    const int dir_index = 1;
     const int perm_index = 2;
     std::string c_dir_name = getFilePath(tokens[dir_index]);
     int permissions = std::stoi(tokens[perm_index], nullptr, 8);
@@ -117,7 +116,7 @@ int AbstractAceRunner::handle_mkdir(std::vector<std::string>& tokens) {
 
 int AbstractAceRunner::handle_open(std::vector<std::string>& tokens) {
     const int file_index = 1;
-	const int flag_index = 2;
+    const int flag_index = 2;
     const int perm_index = 3;
 
     std::string file_path = getFilePath(tokens[file_index]);
@@ -136,13 +135,13 @@ int AbstractAceRunner::handle_open(std::vector<std::string>& tokens) {
 }
 
 int AbstractAceRunner::handle_opendir(std::vector<std::string>& tokens) {
-	tokens.insert(tokens.begin() + 2, "O_DIRECTORY");
-	return handle_open(tokens);
+    tokens.insert(tokens.begin() + 2, "O_DIRECTORY");
+    return handle_open(tokens);
 }
 
 int AbstractAceRunner::handle_remove(std::vector<std::string>& tokens) {
-	const int filenameIndex = 1;
-	return remove(getFilePath(tokens[filenameIndex]).c_str());
+    const int filenameIndex = 1;
+    return remove(getFilePath(tokens[filenameIndex]).c_str());
 }
 
 int AbstractAceRunner::handle_rename(std::vector<std::string>& tokens) {
@@ -150,23 +149,23 @@ int AbstractAceRunner::handle_rename(std::vector<std::string>& tokens) {
 }
 
 int AbstractAceRunner::handle_sync(std::vector<std::string>& tokens) {
-	sync();
-	return 1;
+    sync();
+    return 1;
 }
 
 int AbstractAceRunner::handle_truncate(std::vector<std::string>& tokens) {
-	std::string file_path = getFilePath(tokens[1]);
+    std::string file_path = getFilePath(tokens[1]);
     int length = stoi(tokens[2]);
     return truncate(file_path.c_str(), length);
 }
 
 int AbstractAceRunner::handle_unlink(std::vector<std::string>& tokens) {
-	std::string path = getFilePath(tokens[1]);
-	return unlink(path.c_str()); 
+    std::string path = getFilePath(tokens[1]);
+    return unlink(path.c_str()); 
 }
 
 int AbstractAceRunner::handle_write(std::vector<std::string>& tokens) {
-	const int perm_index = 3;
+    const int perm_index = 3;
     std::string file_path = getFilePath(tokens[1]);
     int fd = fileDescriptors[file_path];
     int count = stoi(tokens[3]);
@@ -219,23 +218,23 @@ int AbstractAceRunner::parse_open_flags(std::string flags) {
 void AbstractAceRunner::reset(void) {
     std::cout << "RESET CALLED" << std::endl;
     //std::cout << paths_added.size() << std::endl;
-	std::set<std::string> filePaths = getAllFilePaths();
-	for (std::string file : filePaths) {
-		remove(file.c_str());
-	}
+    std::set<std::string> filePaths = getAllFilePaths();
+    for (std::string file : filePaths) {
+	remove(file.c_str());
+    }
 }
 
 OracleAceRunner::OracleAceRunner(std::string testDirectory, std::string oracleFile) : AbstractAceRunner(testDirectory) {
-	outputFile = oracleFile;
+    outputFile = oracleFile;
 }
 
 int OracleAceRunner::handle_checkpoint(std::vector<std::string>& tokens) {
-	std::cout << "Checkpoint, saving file state." << std::endl;
-	// Crash happens
-	FSSnapshot snapshot(getAllFilePaths());
-	snapshot.printState(std::cout);
-	snapshot.writeToFile(outputFile);
-	return 1;
+    std::cout << "Checkpoint, saving file state." << std::endl;
+    // Crash happens
+    FSSnapshot snapshot(getAllFilePaths());
+    snapshot.printState(std::cout);
+    snapshot.writeToFile(outputFile);
+    return 1;
 }
 
 CrashAceRunner::CrashAceRunner(std::string testDir) : AbstractAceRunner(testDir) {}
@@ -251,7 +250,7 @@ pid_t get_pid() {
 }
 
 int CrashAceRunner::handle_checkpoint(std::vector<std::string>& tokens) {
-	std::cout << "Checkpoint, crashing from CrashAceRunner." << std::endl;
+    std::cout << "Checkpoint, crashing from CrashAceRunner." << std::endl;
     
     // Kill strata
     pid_t pid = get_pid();
