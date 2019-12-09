01cb62f35ccca4bfcb18379519875f933eca6fbc
Jack Stenglein <jackstenglein@utexas.edu>
2019-12-09 15:26:48 -0600

    Finish test workload

--- src/test_workload.cpp
+++ src/test_workload.cpp
@@ -24,26 +24,34 @@ int main() {
 	close(fd);
 	struct stat result;
 	stat("/mlfs/test/foo", &result);
+	std::cout << "Stats before digestion: " << std::endl;
+	printStats(&result);
 
 #ifdef MLFS
 	shutdown_fs();
+	init_fs();
 #endif
+
+	stat("/mlfs/test/foo", &result);
+	std::cout << "Stats after digestion: " << std::endl;
+	printStats(&result);
+
 	return 0;
 }
 
-void printState(std::ostream& out) const {
-	out << "\t\tDevice: " << snapshot.st_dev << std::endl;
-	out << "\t\tInode: " << snapshot.st_ino << std::endl;
-	out << "\t\tMode: " << snapshot.st_mode << std::endl;
-	out << "\t\tHard Links: " << snapshot.st_nlink << std::endl;
-	out << "\t\tUser ID: " << snapshot.st_uid << std::endl;
-	out << "\t\tGroup ID: " << snapshot.st_gid << std::endl;
-	out << "\t\tRdev: " << snapshot.st_rdev << std::endl;
-	out << "\t\tSize: " << snapshot.st_size << std::endl;
-	out << "\t\tBlock size: " << snapshot.st_blksize << std::endl;
-	out << "\t\tBlocks: " << snapshot.st_blocks << std::endl;
-	out << "\t\tAccess time: " << (long long)snapshot.st_atim.tv_sec << "." << snapshot.st_atim.tv_nsec << std::endl;
-	out << "\t\tModified time: " << (long long)snapshot.st_mtim.tv_sec << "." << snapshot.st_mtim.tv_nsec << std::endl;
-	out << "\t\tChange time: " << (long long)snapshot.st_ctim.tv_sec << "." << snapshot.st_ctim.tv_nsec << std::endl;
+void printStats(struct stat* snapshot) {
+	std::cout << "\t\tDevice: " << snapshot->st_dev << std::endl;
+	std::cout << "\t\tInode: " << snapshot->st_ino << std::endl;
+	std::cout << "\t\tMode: " << snapshot->st_mode << std::endl;
+	std::cout << "\t\tHard Links: " << snapshot->st_nlink << std::endl;
+	std::cout << "\t\tUser ID: " << snapshot->st_uid << std::endl;
+	std::cout << "\t\tGroup ID: " << snapshot->st_gid << std::endl;
+	std::cout << "\t\tRdev: " << snapshot->st_rdev << std::endl;
+	std::cout << "\t\tSize: " << snapshot->st_size << std::endl;
+	std::cout << "\t\tBlock size: " << snapshot->st_blksize << std::endl;
+	std::cout << "\t\tBlocks: " << snapshot->st_blocks << std::endl;
+	std::cout << "\t\tAccess time: " << (long long)snapshot->st_atim.tv_sec << "." << snapshot->st_atim.tv_nsec << std::endl;
+	std::cout << "\t\tModified time: " << (long long)snapshot->st_mtim.tv_sec << "." << snapshot->st_mtim.tv_nsec << std::endl;
+	std::cout << "\t\tChange time: " << (long long)snapshot->st_ctim.tv_sec << "." << snapshot->st_ctim.tv_nsec << std::endl;
 }
 
